097a82164e591ffc7568617b838154e8947f2a54
laura <you@example.com>
2024-08-20 12:54:57 -0500

    hallo :D!

---
+++ sumOfArray/src/sumOfArray/Sumofarray.java
@@ -0,0 +1,21 @@
+package sumOfArray;
+import java.util.Arrays;
+public class Sumofarray {
+
+	public static void main(String[] args) {
+		
+		
+	int sum = 0;
+	int[]aeio= {10,5,2,3};{
+
+	for(int a = 0; a < aeio.length; a++) {
+
+	sum += aeio[a];
+	
+	}
+	
+System.out.println(sum);
+	
+}
+	}
+		}
